5fb038fc68f563dde7e6ea22a210834eab2ae8ec
Spencer Haka <spencer.haka@gmail.com>
2022-09-09 15:18:09 -0600

    complete error handling and clear button

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-var userChoice;
 var phraseSpot = document.querySelector('.phrase');
-var receiveMessage = document.querySelector('.receive-message');
-
-receiveMessage.addEventListener('click', assignPhrase);
-
+var receiveMessageButton = document.querySelector('.receive-message');
+var radioMantra = document.querySelector('.mantra');
+var radioAffirmation = document.querySelector('.affirmation');
+var clearButton = document.querySelector('.clear');
+var userChoice;
 var phraseToDisplay;
+
+receiveMessageButton.addEventListener('click', assignPhrase);
+radioMantra.addEventListener('click', enableButton);
+radioAffirmation.addEventListener('click', enableButton);
+clearButton.addEventListener('click', clearPage)
+
+function enableButton() {
+  receiveMessageButton.classList.remove('disabled');
+  userChoice = document.querySelector('input[name=user-choice]:checked').value;
+}
+
 
 function randomPhrase(array) {
   var randomIndex = randomNumber(array);
@@ -19,14 +30,15 @@ function randomNumber(array) {
 };
 
 function assignPhrase() {
-  userChoice = document.querySelector('input[name=user-choice]:checked').value;
-
+  if (userChoice === undefined) {
+    window.alert('Please select an option.');
+    return;
+  } 
   if (userChoice === "mantra") {
     phraseToDisplay = randomPhrase(mantras);
   } else {
     phraseToDisplay = randomPhrase(affirmations);
   }
-
   displayPhrase();
 }
 
@@ -35,6 +47,11 @@ function displayPhrase() {
   phraseSpot.innerHTML = '';
   p.innerText = phraseToDisplay;
   phraseSpot.appendChild(p);
+
+  clearButton.classList.remove('disabled');
 }
 
-console.log(userChoice);
+function clearPage() {
+  window.location.reload();
+}
+
